175c54dc4d0e460efd9ef6e09b7ad0b3aa489ec1
SHREYASH-2211 <137050866+SHREYASH-2211@users.noreply.github.com>
2025-07-17 17:46:02 +0530

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import {Routes,Route} from 'react-router-dom';
+
+function App() {
+  return (
+    <>
+    Hello world
+    </>
+  );
+}
+
+export default App;
